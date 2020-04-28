d6ef9c5de275dadc08861c8ed920400d5ba01bc6
Pier-Luc Desgagne <pldesgagne@lallemand.com>
2020-04-27 22:47:44 -0400

    Main py file baseline

---
+++ VNTR_NJtree.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 22:39:15 2020
+
+@author: pldesgagne
+
+Description:
+    Blah
+"""
+
+if __name__ == '__main__':
+    pass
+
+
+class NJTreeConstructor():
+    
+    def __init__(source, destination):
+        """
+        """
+        pass
+    
+    
